30695e6d6a4a70cee23bba92833094b74861061a
prime <prime@SAQIB>
2023-12-13 10:05:08 +0500

    Add project files.

---
+++ Migrations/20231212124740_initial create.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace assignment.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Faculty",
+                columns: table => new
+                {
+                    Fid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Deptid = table.Column<int>(type: "int", nullable: false),
+                    Standing = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculty", x => x.Fid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Sid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Major = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Standing = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Sid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    Cid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoomNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fid = table.Column<int>(type: "int", nullable: false),
+                    FacultyFid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Cid);
+                    table.ForeignKey(
+                        name: "FK_Classes_Faculty_FacultyFid",
+                        column: x => x.FacultyFid,
+                        principalTable: "Faculty",
+                        principalColumn: "Fid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enrolled",
+                columns: table => new
+                {
+                    Eid = table.Column<int>(type: "int", nullable: false),
+                    Sid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrolled", x => new { x.Eid, x.Sid });
+                    table.ForeignKey(
+                        name: "FK_Enrolled_Classes_Sid",
+                        column: x => x.Sid,
+                        principalTable: "Classes",
+                        principalColumn: "Cid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Enrolled_Students_Sid",
+                        column: x => x.Sid,
+                        principalTable: "Students",
+                        principalColumn: "Sid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marks",
+                columns: table => new
+                {
+                    MarkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EnrolledCid = table.Column<int>(type: "int", nullable: false),
+                    EnrolledEid = table.Column<int>(type: "int", nullable: false),
+                    EnrolledSid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marks", x => x.MarkId);
+                    table.ForeignKey(
+                        name: "FK_Marks_Enrolled_EnrolledEid_EnrolledSid",
+                        columns: x => new { x.EnrolledEid, x.EnrolledSid },
+                        principalTable: "Enrolled",
+                        principalColumns: new[] { "Eid", "Sid" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_FacultyFid",
+                table: "Classes",
+                column: "FacultyFid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrolled_Sid",
+                table: "Enrolled",
+                column: "Sid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Marks_EnrolledEid_EnrolledSid",
+                table: "Marks",
+                columns: new[] { "EnrolledEid", "EnrolledSid" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marks");
+
+            migrationBuilder.DropTable(
+                name: "Enrolled");
+
+            migrationBuilder.DropTable(
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Faculty");
+        }
+    }
+}
